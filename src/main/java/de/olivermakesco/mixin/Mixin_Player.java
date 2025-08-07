1cc3ae436745b03bef0580b2c87264ed831762a8
Oliver-makes-code <oliver@olivermakesco.de>
2025-08-07 18:31:39 -0500

    Fix sweep attacks

---
+++ src/main/java/de/olivermakesco/mixin/Mixin_Player.java
@@ -0,0 +1,29 @@
+package de.olivermakesco.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import de.olivermakesco.PlayerConfigAttachment;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(Player.class)
+public class Mixin_Player {
+    @WrapOperation(
+            method = "attack",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/entity/player/Player;isAlliedTo(Lnet/minecraft/world/entity/Entity;)Z"
+            )
+    )
+    private boolean isSweepPvpMutual(Player self, Entity entity, Operation<Boolean> original) {
+        if (!(entity instanceof Player other))
+            return original.call(self, entity);
+
+        if (PlayerConfigAttachment.get(self).shouldAcceptPvp && PlayerConfigAttachment.get(other).shouldAcceptPvp)
+            return original.call(self, entity);
+
+        return false;
+    }
+}
